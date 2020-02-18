49f3ea032739a32e604432fc1f66b636582efe41
Jingyu Shi <julieshi@usc.edu>
2020-02-17 17:53:20 -0800

    Remove private key

---
+++ Azure_analyzer/testing.py
@@ -0,0 +1,30 @@
+########### Python 3.2 #############
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '********************************',
+}
+
+params = urllib.parse.urlencode({
+    # Request parameters
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'emotion',
+    'recognitionModel': 'recognition_01',
+    'returnRecognitionModel': 'false',
+    'detectionModel': 'detection_01',
+})
+
+try:
+    conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
+    conn.request("POST", "/face/v1.0/detect?%s" % params, '{"url": "https://i.insider.com/5dcc3df979d7570d633e10ea?width=1100&format=jpeg&auto=webp"}', headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+####################################
