fa1189eb865ce8e4b69059adf156debb139a50db
Xiaoyang Qiao <xiaoyanq@usc.edu>
2020-02-17 17:04:28 -0800

    simplify ins-crawler

--- instagram-crawler/crawler.py
+++ instagram-crawler/crawler.py
@@ -12,30 +12,7 @@ from inscrawler.settings import prepare_override_settings
 
 
 def usage():
-    return """
-        python crawler.py posts -u cal_foodie -n 100 -o ./output
-        python crawler.py posts_full -u cal_foodie -n 100 -o ./output
-        python crawler.py profile -u cal_foodie -o ./output
-        python crawler.py profile_script -u cal_foodie -o ./output
-        python crawler.py hashtag -t taiwan -o ./output
-
-        The default number for fetching posts via hashtag is 100.
-    """
-
-
-def get_posts_by_user(username, number, detail, debug):
-    ins_crawler = InsCrawler(has_screen=debug)
-    return ins_crawler.get_user_posts(username, number, detail)
-
-
-def get_profile(username):
-    ins_crawler = InsCrawler()
-    return ins_crawler.get_user_profile(username)
-
-
-def get_profile_from_script(username):
-    ins_cralwer = InsCrawler()
-    return ins_cralwer.get_user_profile_from_script_shared_data(username)
+    return '''python crawler.py hashtag -t taiwan -o ./output'''
 
 
 def get_posts_by_hashtag(tag, number, debug):
@@ -62,10 +39,9 @@ def output(data, filepath):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Instagram Crawler", usage=usage())
     parser.add_argument(
-        "mode", help="options: [posts, posts_full, profile, profile_script, hashtag]"
+        "mode", help="options: [hashtag]"
     )
     parser.add_argument("-n", "--number", type=int, help="number of returned posts")
-    parser.add_argument("-u", "--username", help="instagram's username")
     parser.add_argument("-t", "--tag", help="instagram's tag name")
     parser.add_argument("-o", "--output", help="output file name(json format)")
     parser.add_argument("--debug", action="store_true")
@@ -76,21 +52,7 @@ if __name__ == "__main__":
 
     override_settings(args)
 
-    if args.mode in ["posts", "posts_full"]:
-        arg_required("username")
-        output(
-            get_posts_by_user(
-                args.username, args.number, args.mode == "posts_full", args.debug
-            ),
-            args.output,
-        )
-    elif args.mode == "profile":
-        arg_required("username")
-        output(get_profile(args.username), args.output)
-    elif args.mode == "profile_script":
-        arg_required("username")
-        output(get_profile_from_script(args.username), args.output)
-    elif args.mode == "hashtag":
+    if args.mode == "hashtag":
         arg_required("tag")
         output(
             get_posts_by_hashtag(args.tag, args.number or 100, args.debug), args.output
