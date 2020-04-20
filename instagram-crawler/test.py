c6b4de1af68d4fcb1ee7ea6625fbd592ec7198bc
Enji Li <enjili@usc.edu>
2020-04-19 20:55:53 -0700

    front end

--- instagram-crawler/test.py
+++ instagram-crawler/test.py
@@ -14,7 +14,8 @@ def index():
 @app.route('/',methods=['POST'])
 def getVal():
     hashtag = request.form['hashtag']
-    tone = output(get_posts_by_hashtag(hashtag, 5, debug=True))
+    numPost = request.form['numLabel']
+    tone = output(get_posts_by_hashtag(hashtag, int(numPost), debug=True))
     print("tone below")
     print(tone)
 
@@ -42,7 +43,7 @@ def getVal():
     else:
         txtnotAvailable = 1
 
-    imganger, imgdisgust, imgfear, imghappiness, imgneutral, imgsadness, imgsurprise, imgnotAvailable = 0,0,0,0,0,0,0,0
+    imganger, imgdisgust, imgfear, imghappiness, imgneutral, imgsadness, imgsurprise, imgcontempt, imgnotAvailable = 0,0,0,0,0,0,0,0,0
 
     if tone[0]:
         imganger = tone[0]['anger']
@@ -52,14 +53,19 @@ def getVal():
         imgneutral = tone[0]['neutral']
         imgsadness = tone[0]['sadness']
         imgsurprise = tone[0]['surprise']
+        imgcontempt = tone[0]['contempt']
+
     else:
         imgnotAvailable = 1
 
-    url = {'url1': tone[2][0], 'url2': tone[2][1], 'url3': tone[2][2], 'url4': tone[2][3], 'url5': tone[2][4]}
-    post = {'post1': tone[3][0], 'post2': tone[3][1], 'post3': tone[3][2], 'post4': tone[3][3], 'post5': tone[3][4]}
+    url = tone[2]
+    post = tone[3]
+    # url = {'url1': tone[2][0], 'url2': tone[2][1], 'url3': tone[2][2], 'url4': tone[2][3], 'url5': tone[2][4], 'url6': tone[2][5], 'url7': tone[2][6], 'url8': tone[2][7], 'url9': tone[2][8]}
+    # post = {'post1': tone[3][0], 'post2': tone[3][1], 'post3': tone[3][2], 'post4': tone[3][3], 'post5': tone[3][4], 'post6': tone[3][5], 'post7': tone[3][6], 'post8': tone[3][7], 'post9': tone[3][8]}
+
     return render_template('final-homepage.html',tone = tone, anger = imganger, disgust = imgdisgust, fear = imgfear,
-                           happiness = imghappiness,neutral = imgneutral, sadness = imgsadness, surprise = imgsurprise,
-                            imgnotAvailable = imgnotAvailable,
+                           happiness = imghappiness, neutral = imgneutral, sadness = imgsadness, surprise = imgsurprise,
+                            contempt = imgcontempt, imgnotAvailable = imgnotAvailable,
                            txtanger = txtanger, txtfear = txtfear, txtjoy = txtjoy, txtsadness = txtsadness,
                            txtanalytical = txtanalytical,
                            txtconfident = txtconfident, txttentative = txttentative, txtnotAvailable = txtnotAvailable,
